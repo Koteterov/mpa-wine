0eb7e3f7ed63d1f6782b9988cc03220ffdab24a3
Dimitar Koteterov <koteterov@yahoo.com>
2022-08-29 16:42:47 +0300

    router and home controller added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const routes = require('./routes')
 
 const app = express();
 
@@ -12,8 +13,6 @@ app.engine("hbs", handlebars.engine({
 app.set("view engine", "hbs");
 app.set("views", "./src/views")
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(routes)
 
 app.listen(5000, () => console.log("Server is listening on port 5000"));
