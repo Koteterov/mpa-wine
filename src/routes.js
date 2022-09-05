cb406f8de6ca96a77850a66df160268c9d58390e
Dimitar Koteterov <koteterov@yahoo.com>
2022-09-05 20:40:46 +0300

    login and register page rendered

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ const homeController = require("./controllers/homeControllers");
 const aboutController = require("./controllers/aboutConroller");
 const wineController = require("./controllers/wineController");
 const accessoryController = require("./controllers/accessoryController");
+const authController = require("./controllers/authController")
 
 const router = express.Router();
 router.get("/", homeController);
 router.get("/about", aboutController);
 router.use("/accessory", accessoryController);
 router.use("/wine", wineController);
+router.use("/auth", authController);
 
 module.exports = router;
