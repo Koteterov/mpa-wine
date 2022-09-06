897ed5e84e4379ab2c612281309e90b31e6a00bb
Dimitar Koteterov <koteterov@yahoo.com>
2022-09-06 09:55:18 +0300

    user model added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,13 @@ router.get("/register", (req, res) => {
     res.render("auth/register")
 })
 
+router.post("/register", (req, res) => {
+    console.log(req.body);
+
+
+    res.redirect("/auth/register")
+})
+
 router.get("/login", (req, res) => {
     res.render("auth/login")
 })
