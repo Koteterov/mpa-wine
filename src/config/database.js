4f7a8c8478005857f97d97a2d962d62203aeb651
Dimitar Koteterov <koteterov@yahoo.com>
2022-09-18 20:05:02 +0300

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// const connectionString = "mongodb://localhost:27017/winesCollection";
-const connectionString = "mongodb+srv://koteterov:TuksamzaAtlas12@mpa-wine.kxcjphs.mongodb.net/?retryWrites=true&w=majority";
+const connectionString = "mongodb://localhost:27017/winesCollection";
+// const connectionString = "mongodb+srv://koteterov:TuksamzaAtlas12@mpa-wine.kxcjphs.mongodb.net/?retryWrites=true&w=majority";
 
 exports.initializeDatabase = () => mongoose.connect(connectionString);
