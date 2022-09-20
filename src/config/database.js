276193a5be1ab87f01f3f83111a5ef57d4753bc4
Dimitar Koteterov <koteterov@yahoo.com>
2022-09-20 11:32:59 +0300

    config

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-const connectionString = process.env.DB_HOST;
+const connectionString = `mongodb+srv://${process.env.DB_USER}mpa-wine.kxcjphs.mongodb.net/winesCollection`;
 
 exports.initializeDatabase = () => mongoose.connect(connectionString);
